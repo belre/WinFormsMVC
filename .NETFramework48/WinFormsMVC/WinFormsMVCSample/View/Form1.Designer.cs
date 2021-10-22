57c9dd67da70863e2b47c989578c3853e822071c
belre <neopossam@gmail.com>
2021-10-22 12:29:57 +0900

    フォームにスライダーとテキストの機能を追加

--- .NETFramework48/WinFormsMVC/WinFormsMVCSample/View/Form1.Designer.cs
+++ .NETFramework48/WinFormsMVC/WinFormsMVCSample/View/Form1.Designer.cs
@@ -34,6 +34,10 @@ namespace WinFormsMVCSample
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -66,17 +70,50 @@ namespace WinFormsMVCSample
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(342, 276);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 19);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 377);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 364);
+            this.ClientSize = new System.Drawing.Size(871, 462);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
             }
 
@@ -85,6 +122,9 @@ namespace WinFormsMVCSample
             private System.Windows.Forms.Button button1;
             private System.Windows.Forms.Button button2;
             private System.Windows.Forms.Button button3;
+            private System.Windows.Forms.TrackBar trackBar1;
+            private System.Windows.Forms.TextBox textBox1;
+            private System.Windows.Forms.TextBox textBox2;
         }
     }
 
--- .NETFramework48/WinFormsMVC/WinFormsMVCSample/View/Form1.cs
+++ .NETFramework48/WinFormsMVC/WinFormsMVCSample/View/Form1.cs
@@ -105,6 +105,81 @@ namespace WinFormsMVCSample
                     }, null);
                 }
             }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                int init_value = 5;
+                textBox1.Text = init_value.ToString();
+                trackBar1.Value = init_value;
+            }
+
+
+            private void trackBar1_ValueChanged(object sender, EventArgs e)
+            {
+                textBox1.Text = trackBar1.Value.ToString();
+            }
+
+            private bool textbox_and_trackbar_HasError(TextBox textbox, TrackBar trackbar)
+            {
+                int value;          // 整数変換の結果
+
+                // もしもテキストボックスの値が、
+                // 整数に変換できない場合はエラーとする。
+                if (!int.TryParse(textbox.Text, out value))
+                {
+                    return true;
+                }
+                
+                // もしもトラックバーの値の最小値、最大値の範囲に、
+                // 整数に変換された値valueが入っていない場合はエラーとする。
+                if (value < trackbar.Minimum || value > trackbar.Maximum)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private void textBox1_TextChanged(object sender, EventArgs e)
+            {
+                if (textbox_and_trackbar_HasError(textBox1, trackBar1))
+                {
+                    textBox1.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    textBox1.BackColor = SystemColors.Window;
+                }
+            }
+
+            private void textBox1_Validating(object sender, CancelEventArgs e)
+            {
+                // 閉じるボタンを押すときにも、textBox1_Validatingが実行される。
+                // 従って、if文がないと、閉じるボタンを押したときに閉じれなくなる。
+                // → いまフォーカスが当たっているコントロールと、イベントを起こしたコントロールが
+                // 同じであれば、現在入力待ちの状態なので、e.Cancel = true;としないようにする。
+                if (ActiveControl.Equals(sender))
+                {
+                    return;
+                }
+
+                if (textbox_and_trackbar_HasError(textBox1, trackBar1))
+                {
+                    // e.Cancelと指定することで、
+                    // 以降の処理をすべて中止する。
+                    e.Cancel = true;
+                }
+            }
+
+            private void textBox1_Validated(object sender, EventArgs e)
+            {
+                if (!textbox_and_trackbar_HasError(textBox1, trackBar1))
+                {
+                    trackBar1.Value = int.Parse(textBox1.Text);
+                }
+            }
         }
     }
 }
