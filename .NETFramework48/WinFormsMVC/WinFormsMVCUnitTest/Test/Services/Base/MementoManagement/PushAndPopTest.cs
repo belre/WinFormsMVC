6af02d5b919a3771e286fbb30f22414c2b5f683a
belre <neopossam@gmail.com>
2021-10-25 16:45:00 +0900

    Mementoテスト

---
+++ .NETFramework48/WinFormsMVC/WinFormsMVCUnitTest/Test/Services/Base/MementoManagement/PushAndPopTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WinFormsMVC.Request;
+using WinFormsMVC.Request.Item;
+using WinFormsMVC.View;
+
+namespace WinFormsMVCUnitTest.Test.Services.Base.MementoManagement
+{
+    [TestClass]
+    public class PushAndPopTest
+    {
+        private WinFormsMVC.Services.Base.MementoManagement _managed_memento;
+
+        protected Command[] SingleDefaultCommand
+        {
+            get
+            {
+                return new List<Command>()
+                {
+                    new GenericCommand<BaseForm, TextItem>()
+                    {
+                        Validation = (item) =>
+                        {
+                            item.Next = "Test Text 1";
+                            return true;
+                        }
+                    }
+                }.ToArray();
+            }
+        }
+
+        public PushAndPopTest()
+        {
+            _managed_memento = new WinFormsMVC.Services.Base.MementoManagement();
+        }
+
+        [TestMethod]
+        public void PushSomeQueries()
+        {
+            _managed_memento.PushCommand(SingleDefaultCommand);
+            _managed_memento.PushCommand(SingleDefaultCommand);
+            _managed_memento.PushCommand(SingleDefaultCommand);
+
+            Assert.AreEqual(3, _managed_memento.Mememtoes.Count);
+        }
+
+        [TestMethod]
+        public void PushManyQueries()
+        {
+            for (int i = 0; i < _managed_memento.MAX_MEMEMTO_NUMBER-1; i++)
+            {
+                _managed_memento.PushCommand(SingleDefaultCommand);
+            }
+
+            Assert.AreEqual(_managed_memento.MAX_MEMEMTO_NUMBER-1, _managed_memento.Mememtoes.Count);
+
+            _managed_memento.PushCommand(SingleDefaultCommand);
+            Assert.AreEqual(_managed_memento.MAX_MEMEMTO_NUMBER, _managed_memento.Mememtoes.Count);
+
+            _managed_memento.PushCommand(SingleDefaultCommand);
+            Assert.AreEqual(_managed_memento.MAX_MEMEMTO_NUMBER, _managed_memento.Mememtoes.Count);
+
+        }
+
+    }
+}
